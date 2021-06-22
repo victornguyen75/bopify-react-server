c1d1ee258bc28b6abdd921abde1524bd913769d7
Nguyen, Victor <22437776+victornguyen75@users.noreply.github.com>
2021-06-22 10:42:22 -0500

    feat(): add server setup for authentication and authorization

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const SpotifyWebApi = require("spotify-web-api-node");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.post("login", (req, res) => {
+  const code = req.body.code;
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: "http://localhost:3000",
+    clientId: "e3c52dc073bb460cbabfabfdf10c4463",
+    clientSecret: "4533703e957c43e3b8b8ec692cff71d7",
+  });
+
+  spotifyApi
+    .authorizationCodeGrant(code)
+    .then((data) => {
+      res.json({
+        accessToken: data.body.access_token,
+        refreshToken: data.body.refresh_token,
+        expiresIn: data.body.expires_in,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(400);
+    });
+});
+
+app.listen(3001);
